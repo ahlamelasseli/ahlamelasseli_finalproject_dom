409bdbe0bd0ef6d785a6d1ca02a5ea2eb21f45dd
ahlam <ahlamelasseli>
2025-05-09 16:24:26 +0100

    update btnvideo

--- public/js/app.js
+++ public/js/app.js
@@ -1,37 +1,20 @@
 
 
-const headerJs = () => {
-  let menuBtn = document.querySelector("#open")
-  let closeBtn = document.querySelector("#close")
-  let navBar = document.querySelector(".navLinks")
-  let dropDown = document.querySelector(".dropDownDiv");
-  let dropDownSelect = document.querySelector(".dropDownSelect");
-  let openModal = document.querySelector(".openModal")
-  let closeModal = document.querySelector(".closeModal")
-  let modalDiv = document.querySelector(".modalDiv");
-  menuBtn.addEventListener("click", () => {
-    navBar.style.top = "50%";
-  });
-  closeBtn.addEventListener("click", () => {
-    navBar.style.top = "-200%";
-  });
-
-  dropDown.addEventListener("mouseover", () => {
-    dropDownSelect.classList.remove("hidden");
-  });
-  dropDownSelect.addEventListener("mouseout", () => {
-    dropDownSelect.classList.add("hidden");
-  });
-  openModal.addEventListener("click", () => {
-    modalDiv.style.top = "2vh";
-  });
-  closeModal.addEventListener("click", () => {
-    modalDiv.style.top = "-200%";
-  });
-};
-headerJs();
+let vd = document.getElementById("vd");
+let i4la9e = document.getElementById("i4la9e");
+let ffllex = document.getElementById("ffllex");
 
 
+vd.addEventListener("click", (e) => {
+    e.preventDefault(); 
+    ffllex.style.display = "block"; 
+    body.style.overflow = "hidden"; 
+});
+
+i4la9e.addEventListener("click", () => {
+    ffllex.style.display = "none"; 
+    body.style.overflow = "auto"; 
+});
 
 
 
