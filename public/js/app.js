e71c9931ccb2fbc56ddf75878843c32c79b99890
ahlam <ahlamelasseli>
2025-05-09 12:39:34 +0100

    events

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,40 @@
+
+
+const headerJs = () => {
+  let menuBtn = document.querySelector("#open")
+  let closeBtn = document.querySelector("#close")
+  let navBar = document.querySelector(".navLinks")
+  let dropDown = document.querySelector(".dropDownDiv");
+  let dropDownSelect = document.querySelector(".dropDownSelect");
+  let openModal = document.querySelector(".openModal")
+  let closeModal = document.querySelector(".closeModal")
+  let modalDiv = document.querySelector(".modalDiv");
+  menuBtn.addEventListener("click", () => {
+    navBar.style.top = "50%";
+  });
+  closeBtn.addEventListener("click", () => {
+    navBar.style.top = "-200%";
+  });
+
+  dropDown.addEventListener("mouseover", () => {
+    dropDownSelect.classList.remove("hidden");
+  });
+  dropDownSelect.addEventListener("mouseout", () => {
+    dropDownSelect.classList.add("hidden");
+  });
+  openModal.addEventListener("click", () => {
+    modalDiv.style.top = "2vh";
+  });
+  closeModal.addEventListener("click", () => {
+    modalDiv.style.top = "-200%";
+  });
+};
+headerJs();
+
+
+
+
+
 let modi = document.getElementById("modi")
 let praesentium = document.getElementById("praesentium")
 let pariatur = document.getElementById("pariatur")
@@ -133,3 +170,58 @@ document.getElementById('reservation-form').addEventListener('submit', function 
       alert('Table booked successfully!');
     }
   });
+  //^ events
+
+  let nextBtns = document.querySelectorAll(".next-btn");
+let previousBtns = document.querySelectorAll(".previous-btn");
+let containers = document.querySelectorAll(".carousel-container");
+let carouselIndexes = new Map();
+
+nextBtns.forEach(btn => {
+    btn.addEventListener("click", (e) => {
+        let target = e.target.getAttribute("carousel-target");
+        let targetID = carouselIndexes.get(target) || 0;
+        slider(targetID + 1, target);
+    });
+});
+
+previousBtns.forEach(btn => {
+    btn.addEventListener("click", (e) => {
+        let target = e.target.getAttribute("carousel-target");
+        let targetID = carouselIndexes.get(target) || 0;
+        slider(targetID - 1, target);
+    });
+});
+
+
+setInterval(() => {
+    containers.forEach(container => {
+        if (container.getAttribute("autoslide") === "true") {
+            let id = container.id;
+            let currentIndex = carouselIndexes.get(id) || 0;
+            slider(currentIndex + 1, id);
+        }
+    });
+}, 3000); 
+
+
+const slider = (index, target) => {
+    containers.forEach(container => {
+        if (container.id == target) {
+            let camera = container.querySelector(".carousel");
+            let slide = container.querySelectorAll(".slide");
+            let slideWidth = slide[0].clientWidth;
+
+            if (index < 0) {
+                index = slide.length - 1;
+            } else if (index >= slide.length) {
+                index = 0;
+            }
+
+            camera.style.transform = `translateX(-${slideWidth * index}px)`;
+            carouselIndexes.set(target, index);
+        }
+    });
+};
+
+// 
